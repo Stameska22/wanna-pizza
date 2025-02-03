3275db2e9d516dcd5ddbf77959e211734efa3bb8
Victoria Zamana <137291570+Stameska22@users.noreply.github.com>
2025-02-03 11:34:45 +0100

    Initial commit

---
+++ src/app/Login/VerifyModal.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { useAppDispatch } from "../../redux/storeTypes";
+import styles from "./AuthForm.module.scss";
+import verifyImg from "../../assets/img/verify-email.png";
+
+interface ModalProps {
+  email: string;
+  onClose: () => void;
+}
+
+const VerifyModal: React.FC<ModalProps> = ({ email, onClose }) => {
+  return (
+    <>
+      <div className={styles["verify-overlay"]} onClick={onClose}></div>
+      <div className={styles["verify"]}>
+        <img src={verifyImg} />
+        <h2>Verify your account</h2>
+
+        <p>
+          Follow the link from the message sent to your address:{" "}
+          <strong>{email}</strong>
+        </p>
+        <div className={styles["buttons"]}>
+          <button
+            className={styles["button-ok"]}
+            onClick={() => {
+              onClose();
+            }}
+          >
+            <span>Okay</span>
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default VerifyModal;
